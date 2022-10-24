d9c12b33eb8d1b6ce64ef2a18d9da5ea2752d94c
Simer <singhsimer09@gmail.com>
2022-10-23 19:34:53 -0700

    starting client side

---
+++ client/src/Components/forms/AddCar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Form, Input } from 'antd'
+
+const AddCar = () => {
+    return (
+        // <Form>
+        //     <Form.Item name="firstName"
+        //         rules={[{ required: true, message: "Please input your first name!" }]}>
+        //         <Input placeholder="i.e. john" />
+        //     </Form.Item>
+        //     <Form.Item name="lastName"
+        //         rules={[{ required: true, message: "Please input your last name!" }]}>
+        //         <Input placeholder="i.e. smith" />
+        //     </Form.Item>
+        // </Form>
+        <>
+        </>
+    )
+}
+
+export default AddCar
